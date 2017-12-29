11ec31610ccb61176c04deccd9e66efa95cfb3b8
Greg Toombs <reinderien@users.noreply.github.com>
2017-12-29 10:03:27 -0500

    Create replicon.js

---
+++ replicon.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        replicon
+// @version     0.1
+// @description Fix Chrome incompatibilities in Replicon TimeSheet sites
+// @author      Greg Toombs
+// @include     /^https?://timesheet\.\w+\.\w+/Timesheet/main\.aspx$/
+// @run-at      document-end
+// @grant       unsafeWindow
+// ==/UserScript==
+
+// Patch the incompatible load element - replicon expects it to be a window, not an iframe
+var load_iframe = document.getElementById('load');
+unsafeWindow.load = load_iframe.contentWindow;
+
+// Patch the parameter parsing in jsLoaderObject.loadFrom() - inline comments break it
+unsafeWindow.nocomments = function(s) {
+   return s.replace(/\s*\/\*.*\*\/\s*/, '');
+};
+var lf = unsafeWindow.jsLoaderObject_loadFrom.toString()
+   .replace('function jsLoaderObject_loadFrom',
+            'window.jsLoaderObject_loadFrom = function')
+   .replace(/var body/g, 'o = nocomments(o); var body');
+unsafeWindow.eval(lf);
+unsafeWindow.jsLoader.jsLoaderObject.loadFrom = unsafeWindow.jsLoaderObject_loadFrom;
