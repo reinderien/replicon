e2c429c2165a57f38fbed9466ad4cc6e35d52ac6
Greg Toombs <reinderien@users.noreply.github.com>
2017-12-30 15:21:56 -0500

    Fix for Firefox

--- replicon.user.js
+++ replicon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        replicon
-// @version     0.3
-// @description Fix Chrome incompatibilities in Replicon TimeSheet sites
+// @version     0.4
+// @description Fix Chrome+Firefox incompatibilities in Replicon TimeSheet sites
 // @author      Greg Toombs
 // @namespace   https://github.com/reinderien
 // @homepage    https://github.com/reinderien/replicon
@@ -17,7 +17,13 @@ window.load = document.getElementById('load').contentWindow;
 old_string = window.String;
 window.String = function(o) {
     var s = old_string(o);
-    if (typeof(o) == 'function')
+    if (typeof(o) == 'function') {
+        // Chrome fix
         s = s.replace(/\s*\/\*.*?\*\/\s*/g, '');
+        // Firefox fix
+        var close = s.indexOf(')'),
+            params = s.substr(0, close).replace('\n', '');
+        s = params + s.substr(close);
+    }
     return s;
 };
